556617cfd21791f847dbcabad68ce01946cfb681
henry034 <henryyeh034@gmail.com>
2019-09-03 21:35:32 +0800

    add log, remove redundant file

--- api.py
+++ api.py
@@ -9,13 +9,22 @@ app = flask.Flask(__name__)
 CORS(app, supports_credentials=True)
 txt_path = './ntpu-tts/test.txt'
 wav_path = './ntpu-tts/output.wav'
+log_path = './logs/log.txt'
 cmd = './run_gmm.sh'
+
+def tts_log(data, stdout):
+    log_str = '[TEXT] {} \t[SR] {}\n[STDOUT] {}\n'.format(
+                data['text'], data['SR'], stdout)
+    with open(log_path, 'a') as f:
+        f.write(log_str)
+
+        
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Hello</h1>"
 
-@app.route('/tts', methods=['POST'])
+@app.route('/tts2', methods=['POST'])
 def tts():
     if not request.json:
         abort(400)
@@ -27,11 +36,11 @@ def tts():
         SR = float(data['SR'])
         ISR = 1/SR
         stdout = check_output([cmd, str(ISR)]).decode('utf-8')
-
+        tts_log(data, stdout)
         return stdout
 
 
-@app.route('/tts2', methods=['POST'])
+@app.route('/tts', methods=['POST'])
 def tts2():
     if not request.json:
         abort(400)
